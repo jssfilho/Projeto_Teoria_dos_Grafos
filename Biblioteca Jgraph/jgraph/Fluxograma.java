90a3a2f85dd7888d4d3adf3396af96abd580ad47
JardelBrandon <jardelbrandon@hotmail.com>
2018-03-02 16:31:11 -0300

    Exemplos de códigos
    Adicionando código para criação do grafo, para servir de base para nosso projeto

---
+++ Biblioteca Jgraph/jgraph/Fluxograma.java
@@ -0,0 +1,13 @@
+package br.ucsal.jgraph;
+
+
+public class Fluxograma {
+	public Fluxograma(){
+		Acoes a = new Acoes();
+		a.setVisible(true);
+	}
+	
+	public static void main (String args[]){
+		new Fluxograma();
+	}
+}
---
+++ Biblioteca Jgraph/jgraph/Principal.java
@@ -0,0 +1,13 @@
+package br.ucsal.jgraph;
+
+
+public class Principal {
+	public Principal(){
+		Acoes a = new Acoes();
+		a.setVisible(true);
+	}
+	
+	public static void main (String args[]){
+		new Principal();
+	}
+}
