f96df368d7787b84edce7a061202933ff0bf8e93
jssfilho <30275261+jssfilho@users.noreply.github.com>
2018-03-08 20:21:21 -0300

    ....

--- MapaFX.java
+++ MapaFX.java
@@ -26,65 +26,11 @@ public class MapaFX extends Application {
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Gerar Mapa");
-        
-        GeradorMapa mapaJogo = new GeradorMapa();
-        mapaJogo.gerarCaminhos();
-        Group componentes = new Group(); 
-        
-        int n = mapaJogo.mapa.size();
-        int n_maior = mapaJogo.maiorNumeroVertice;
-        int largura, altura, v;
-        int lastX = 110, lastY = 110;
-        
-        altura = n_maior*20 + (n_maior-1)*60+lastX+lastY;
-        
-        largura = n*20 + (n-1)*100 + lastX+lastY;
-        
-        
-        int pInicial; //não está usando
-        for(int cont= 0;cont<n;cont++){
-            
-            v = mapaJogo.mapa.get(cont).length;
-            if (v==1){
-              lastY = altura/2;
-            }else {
-              lastY = (altura/2)-((v-1)*60)/2;
-            }
-            for(int i = 0; i<v;i++){
-                if (v==1){
-                    Circle verticeX = new Circle(10);
-                    verticeX.setTranslateX(lastX);
-                    verticeX.setTranslateY(lastY);
-                    verticeX.setFill(Color.BROWN);
-                    componentes.getChildren().add(verticeX);
-                }else{
-                    Circle verticeX = new Circle(10);
-                    verticeX.setTranslateX(lastX);
-                    verticeX.setTranslateY(lastY);
-                    verticeX.setFill(Color.BROWN);
-                    componentes.getChildren().add(verticeX);
-                }  
-                lastY+=60;
-            }
-            lastX+=100;
-        }
-       
-        
-        
-        
-        Line linhaTeste = new Line();
-        linhaTeste.setStartX(0);
-        linhaTeste.setStartY(0);
-        linhaTeste.setEndX(largura);
-        linhaTeste.setEndY(altura);
-        linhaTeste.setFill(Color.BLACK);
-        componentes.getChildren().add(linhaTeste);
-        Scene cena = new Scene(componentes, largura, altura);
-        cena.setFill(Color.LIGHTBLUE);
+      
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                primaryStage.setScene(cena);
+                primaryStage.setScene(Grafo());
             }
         });
         
@@ -104,4 +50,77 @@ public class MapaFX extends Application {
         launch();
     }
     
+    public Scene Grafo(){
+        GeradorMapa mapaJogo = new GeradorMapa();
+        mapaJogo.gerarCaminhos();
+        Group componentes = new Group(); 
+        
+        int n = mapaJogo.mapa.size();
+        int n_maior = mapaJogo.maiorNumeroVertice;
+        int largura, altura, v;
+        int lastX = 110, lastY = 110;
+        
+        altura = n_maior*20 + (n_maior-1)*60+lastX+lastY;
+        
+        largura = n*20 + (n-1)*100 + lastX+lastY;
+     
+        for(int cont= 0;cont<n;cont++){
+            
+            v = mapaJogo.mapa.get(cont).length;
+            if (v==1){
+              lastY = altura/2;
+            }else {
+              lastY = (altura/2)-((v-1)*60)/2;
+            }
+            for(int i = 0; i<v;i++){
+                if (v==1){
+                    Circle verticeX = new Circle(10);
+                    verticeX.setTranslateX(lastX);
+                    verticeX.setTranslateY(lastY);
+                    verticeX.setFill(Color.BROWN);
+                    componentes.getChildren().add(verticeX);
+                    mapaJogo.mapa.get(cont)[i].v=verticeX;
+                }else{
+                    Circle verticeX = new Circle(10);
+                    verticeX.setTranslateX(lastX);
+                    verticeX.setTranslateY(lastY);
+                    verticeX.setFill(Color.BROWN);
+                    componentes.getChildren().add(verticeX);
+                    mapaJogo.mapa.get(cont)[i].v=verticeX;
+                }  
+                lastY+=60;
+            }
+            lastX+=100;
+        }
+        
+        int n_caminhos = mapaJogo.caminhos.size();
+        for(int w=0;w<n_caminhos;w++){
+            for(int i=0;i<mapaJogo.caminhos.get(w).length;i++){
+                for(int j=0;j<mapaJogo.caminhos.get(w)[i].length;j++){
+                    if (mapaJogo.caminhos.get(w)[i][j]==1){
+                        System.out.println("nada");
+                        Line arestaX = new Line();
+                        arestaX.setStartX(mapaJogo.mapa.get(w)[i].v.getTranslateX()+10);
+                        arestaX.setStartY(mapaJogo.mapa.get(w)[i].v.getTranslateY());
+                        arestaX.setEndX(mapaJogo.mapa.get(w+1)[j].v.getTranslateX()-10);
+                        arestaX.setEndY(mapaJogo.mapa.get(w+1)[j].v.getTranslateY());
+                        arestaX.setFill(Color.CHOCOLATE);
+                        componentes.getChildren().add(arestaX);
+                    }
+                }
+            }
+        }
+        /*
+        Line arestaX = new Line();
+        arestaX.setStartX(mapaJogo.mapa.get(0)[0].v.getTranslateX());
+        arestaX.setStartY(mapaJogo.mapa.get(0)[0].v.getTranslateY());
+        arestaX.setEndX(mapaJogo.mapa.get(1)[0].v.getTranslateX());
+        arestaX.setEndY(mapaJogo.mapa.get(1)[0].v.getTranslateY());
+        arestaX.setFill(Color.CHOCOLATE);
+        componentes.getChildren().add(arestaX);
+        */
+        Scene cena = new Scene(componentes, largura, altura);
+        cena.setFill(Color.LIGHTBLUE);
+        return cena;
+    }
 }
